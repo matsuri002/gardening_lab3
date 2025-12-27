4e1facada89697d73df7b38f704d534df091d533
maturi002 <oimoimoimo09@gmail.com>
2025-12-27 22:55:47 +0900

    戻るボタンの追加 #3

--- src/pages/SelectPlanterPage.tsx
+++ src/pages/SelectPlanterPage.tsx
@@ -29,10 +29,15 @@ export default function SelectPlanterPageContainer() {
           disableGutters
           sx={{ px: { xs: 2, sm: 3, md: 4 }, py: { xs: 2, sm: 3 } }}
         >
+        
+        {/* 戻るボタン押下後TopPageに画面遷移 */}
+        <Button size='large' sx={{ p: { xs: 2.5, sm: 3 } }}> 戻る </Button>
+
           <Box component='footer' sx={{ p: 2, textAlign: 'center', color: 'text.secondary' }}>
             <Typography variant='h5'>コマツナを選択</Typography> {/* TODO:コマツナの部分はdataから持ってくる*/}
             <Typography variant='h5'>データの確認をしたい鉢を選択してください</Typography>
           </Box>
+          
           {/* 鉢の表示 */} {/* TODO:表示する鉢はdataから持ってくる*/}
           <Box sx={{display: 'flex', gap: 2, width: '100%', justifyContent: 'center' }}>
             <Button
