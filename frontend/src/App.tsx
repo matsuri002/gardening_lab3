e9c1eb5de0e02fb28032335b7dc3ddca2a051b0d
maturi002 <oimoimoimo09@gmail.com>
2026-01-02 11:39:59 +0900

    frontendフォルダの作成と移動 #5

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+// import TopPageContainer from "./pages/TopPage";
+// import SelectPlanterPageContainer from "./pages/SelectPlanterPage";
+// import DailyRecordPageContainer from "./pages/DailyRecordPage";
+// import WeeklyRecordPageContainer from "./pages/WeeklyRecordPage";
+import PhotoPageContainer from "./pages/PhotoPage";
+
+export default function App() {
+  return <PhotoPageContainer />
+}
