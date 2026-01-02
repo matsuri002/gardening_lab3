40e00c43848bf41b1659690b0a7ad34e0653cda4
maturi002 <oimoimoimo09@gmail.com>
2026-01-02 11:56:00 +0900

    frontendフォルダ作成時の未コミット #5

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-// import TopPageContainer from "./pages/TopPage";
-// import SelectPlanterPageContainer from "./pages/SelectPlanterPage";
-// import DailyRecordPageContainer from "./pages/DailyRecordPage";
-// import WeeklyRecordPageContainer from "./pages/WeeklyRecordPage";
-import PhotoPageContainer from "./pages/PhotoPage";
-
-export default function App() {
-  return <PhotoPageContainer />
-}
